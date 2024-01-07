7ce2f3c7404f045cbc37e2597ba12e46ff9eb4f7
jack <1508623784@qq.com>
2024-01-07 21:27:26 +0800

    update-0107

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
-  <div id="app">
-    APP组件
+  <div>
+    <Header/>
+    <router-view></router-view>
+    <Footer/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
+
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less" scoped>
+
 </style>
